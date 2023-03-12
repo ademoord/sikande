b992b99071a25b1cd0dc34863a2953af8cadb671
andromedasupendi <andromedasupendi@gmail.com>
2023-03-12 20:25:58 +0700

    new unittest example to test Jenkins job

---
+++ tests/test_reports.py
@@ -0,0 +1,16 @@
+import unittest
+from app import app
+
+class TestReportsRoute(unittest.TestCase):
+
+    def setUp(self):
+        self.app = app.test_client()
+        self.app.testing = True
+
+    def test_reports_route(self):
+        response = self.app.get('/reports')
+        self.assertEqual(response.status_code, 200)
+
+if __name__ == '__main__':
+    unittest.main()
+
