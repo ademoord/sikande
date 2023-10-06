1337e9e072167d204fc67545027f54767eb1f4ce
andromedasupendi <andromedasupendi@gmail.com>
2023-10-06 03:03:17 +0000

    Separate the models to make it modular-based

--- flask_app.py
+++ flask_app.py
@@ -6,45 +6,10 @@
 import datetime
 import helpers
 from flask import render_template, request, redirect, url_for, flash
-from flask_login import UserMixin, login_user, logout_user, login_required, current_user
+from flask_login import login_user, logout_user, login_required, current_user
 from config import app, db, login_manager
+from models import Item, Debt, User
 
-# START OF MODELS CREATION
-# Item model
-class Item(db.Model):
-    itemID = db.Column(db.Integer, primary_key=True)
-    itemName = db.Column(db.String(64), index=True, unique=False)
-    itemPrice = db.Column(db.Integer, index=True)
-    itemTimestamp = db.Column(db.DateTime, index=True)
-
-    def __repr__(self):
-        return '<Item {}>'.format(self.itemName)
-
-# Debt model
-class Debt(db.Model):
-    debtID = db.Column(db.Integer, primary_key=True)
-    debtName = db.Column(db.String(64), index=True, unique=False)
-    debtTotal = db.Column(db.Integer, index=True)
-    debtCreditor = db.Column(db.String(64), index=True, unique=False)
-    debtReceived = db.Column(db.DateTime, index=True)
-    debtDeadline = db.Column(db.DateTime, index=True)
-
-    def __repr__(self):
-        return '<Debt {}>'.format(self.debtName)
-
-# User model
-class User(db.Model, UserMixin):
-    id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(20), unique=True, nullable=False)
-    password = db.Column(db.String(60), nullable=False)
-
-    def __init__(self, username, password):
-        self.username = username
-        self.password = password
-
-
-# END OF MODELS CREATION
-# --------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 # START OF VIEW AND CONTROLLER SECTION
 
 # Create a global var for the needs of time adjustment
